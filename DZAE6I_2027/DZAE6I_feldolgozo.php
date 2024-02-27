40fc89dc49a98dc7e85b37270254cc721675269d
ARTidas <artidas@artidas.hu>
2024-02-27 16:53:34 +0100

    Removed autoplay from video. Added string length checking.

--- DZAE6I_2027/DZAE6I_feldolgozo.php
+++ DZAE6I_2027/DZAE6I_feldolgozo.php
@@ -32,22 +32,32 @@
                 $confidence = $_POST['confidence'];
 
                 if (empty($name)) {
-                    echo "<span class=\"warning\">Name cannot by empty!</span><br>";
+                    echo "<span class=\"warning\">Name cannot be empty!</span><br>";
+                }
+                else {
+                    if (strlen($name) > 50) {
+                        echo "<span class=\"warning\">Name cannot be longer than 50 characters!</span><br>";
+                    }
                 }
                 if (empty($pin)) {
-                    echo "<span class=\"warning\">Pin cannot by empty!</span><br>";
+                    echo "<span class=\"warning\">Pin cannot be empty!</span><br>";
+                }
+                else {
+                    if (strlen($pin) > 50) {
+                        echo "<span class=\"warning\">Pin cannot by longer than 50 characters!</span><br>";
+                    }
                 }
                 if (empty($fruit)) {
-                    echo "<span class=\"warning\">Fruit cannot by empty!</span><br>";
+                    echo "<span class=\"warning\">Fruit cannot be empty!</span><br>";
                 }
                 if (empty($age)) {
-                    echo "<span class=\"warning\">Age cannot by empty!</span><br>";
+                    echo "<span class=\"warning\">Age cannot be empty!</span><br>";
                 }
                 if (empty($shoeSize)) {
-                    echo "<span class=\"warning\">Shoe size cannot by empty!</span><br>";
+                    echo "<span class=\"warning\">Shoe size cannot be empty!</span><br>";
                 }
                 if (empty($confidence)) {
-                    echo "<span class=\"warning\">Confidence cannot by empty!</span><br>";
+                    echo "<span class=\"warning\">Confidence cannot be empty!</span><br>";
                 }
 
                 echo "Name: $name<br>";
