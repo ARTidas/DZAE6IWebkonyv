335e21fdc408a0ea981a6c426bc7af3cc728f35d
Veres Zoltán <artidas@artidas.hu>
2024-03-05 16:58:32 +0100

    Finished third task.

--- DZAE6I_0305/3.js
+++ DZAE6I_0305/3.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     console.log("Starting up engines...");
 
+    $('.container input').css('background-color', '#fff');
+    $('.container input').css('border', '1px solid #fff');
+
     $('.container h1').hover(function () {
         console.log('Mosue over header...');
         alert('Mouse over header...');
@@ -14,11 +17,18 @@ $(document).ready(function() {
         $('.container p').eq(1).hide();
     });
 
-    $('.container button').click(function () {
+    $('.container button').hover(function () {
         console.log('Mouse over button...');
         alert('Mouse over header...');
     });
 
-    
+    $('.container input').hover(function () {
+        $(this).css('border', '1px solid #f00');
+    });
+
+    $('.container input').click(function () {
+        $(this).css('background-color', '#0f0');
+    });
+
     
 });
