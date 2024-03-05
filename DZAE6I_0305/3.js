106da4021473e807bf7ece2da6204bd1123f57eb
Veres Zoltán <artidas@artidas.hu>
2024-03-05 16:48:15 +0100

    Added strong tags for the task to reflect the requirements.

--- DZAE6I_0305/3.js
+++ DZAE6I_0305/3.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     console.log("Starting up engines...");
 
     $('.container h1').hover(function () {
-        console.log('Mosue over...');
+        console.log('Mosue over header...');
+        alert('Mouse over header...');
     });
 
     $('.container p:first strong').click(function () {
@@ -12,5 +13,12 @@ $(document).ready(function() {
     $('.container p').eq(1).children('strong').dblclick(function() {
         $('.container p').eq(1).hide();
     });
+
+    $('.container button').click(function () {
+        console.log('Mouse over button...');
+        alert('Mouse over header...');
+    });
+
+    
     
 });
