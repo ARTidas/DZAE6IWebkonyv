596564e750df8a00b61f0c27c0fa6a526ae724f3
Veres Zoltán <artidas@artidas.hu>
2024-04-02 14:52:35 +0200

    Done with the second task.

--- DZAE6I_0402/2.js
+++ DZAE6I_0402/2.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
     $('#container').html('');
     
-
+    $('#container').append(`<hr/><h2>Foreach solution</h2>`);
     VZObj.cars.forEach((car) => {
         $('#container').append(`
             <h3>${car.name}</h3>
@@ -35,5 +35,23 @@ $(document).ready(function() {
                 </ul>
         `);
     });
+
+    $('#container').append(`<hr/><h2>For solution</h2>`);
+    for (let i = 0; i < VZObj.cars.length; i++) {
+        $('#container').append(`
+            <h3>${VZObj.cars[i].name}</h3>
+                <ul>
+        `);
+
+        for (let j = 0; j < VZObj.cars[i].models.length; j++) {
+            $('#container').append(`
+                <li>${VZObj.cars[i].models[j]}</li>
+            `);
+        }
+
+        $('#container').append(`
+                </ul>
+        `);
+    }
 
 });
