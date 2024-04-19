76811aeed1b4e347d90a38402675d4ef0363f053
Veres Zoltán <artidas@artidas.hu>
2024-04-19 18:57:05 +0200

    Initialized the WebProg (final) project.

--- DZAE6I_0416/DZAE6I_11.js
+++ DZAE6I_0416/DZAE6I_11.js
@@ -3,10 +3,21 @@ $(document).ready(function() {
 
     $('#compute').click(function() {
         console.log('Computing result...');
+        console.log($('#input_a').val());
+        console.log($('#input_b').val());
 
         switch($('input[name="operation"]:checked').val()) {
             case 'multiplication':
-                $('#result').html('... TODO');
+                $('#result').html( $('#input_a').val() * $('#input_b').val() );
+                break;
+            case 'division':
+                $('#result').html( $('#input_a').val() / $('#input_b').val() );
+                break;
+            case 'addition':
+                $('#result').html( $('#input_a').val() + $('#input_b').val() );
+                break;
+            case 'subtraction':
+                $('#result').html( $('#input_a').val() - $('#input_b').val() );
                 break;
             default:
                 console.log('Cannot understand the operation...');
