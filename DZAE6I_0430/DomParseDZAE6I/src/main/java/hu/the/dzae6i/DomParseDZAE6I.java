8da855c46d34195ec7fe65e03c9f0a8d87f7cd21
Veres Zoltán <artidas@artidas.hu>
2024-04-30 16:14:06 +0200

    Was able to read the XML file and started displaying its content on the elements level.

--- DZAE6I_0430/DomParseDZAE6I/src/main/java/hu/the/dzae6i/DomParseDZAE6I.java
+++ DZAE6I_0430/DomParseDZAE6I/src/main/java/hu/the/dzae6i/DomParseDZAE6I.java
@@ -1,8 +1,13 @@
 package hu.the.dzae6i;
 
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
 public class DomParseDZAE6I {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+        DomReadDZAE6I dom_read = new DomReadDZAE6I();
     }
+    
 }
--- DZAE6I_0430/DomParseDZAE6I/src/main/java/hu/the/dzae6i/DomReadDZAE6I.java
+++ DZAE6I_0430/DomParseDZAE6I/src/main/java/hu/the/dzae6i/DomReadDZAE6I.java
@@ -1,5 +1,49 @@
 package hu.the.dzae6i;
 
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 public class DomReadDZAE6I {
+    
+    public DomReadDZAE6I() throws SAXException, IOException, ParserConfigurationException {
+        try {
+            //File xml_file = new File("DZAE6I_orarend.xml");
+            File xml_file = new File("C:\\VZGit\\DZAE6IWebkonyv\\DZAE6I_0430\\DomParseDZAE6I\\src\\main\\java\\hu\\the\\dzae6i\\DZAE6I_orarend.xml");
+            DocumentBuilderFactory document_builder_factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder document_builder = document_builder_factory.newDocumentBuilder();
+            org.w3c.dom.Document xml_document = document_builder.parse(xml_file);
+            xml_document.getDocumentElement().normalize();
+
+            System.out.println("Root node: " + xml_document.getDocumentElement().getNodeName());
+
+            NodeList node_list = xml_document.getElementsByTagName("event");
+            System.out.println("Found nodes: " + node_list.getLength());
+
+            for (int i = 0; i < node_list.getLength(); i++) {
+                Node node = node_list.item(i);
+                
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    System.out.println("ID: " + element.getAttribute("id"));
+                    System.out.println("Course name: " + element.getElementsByTagName("course_name").item(0).getTextContent());
+                }
+            }
+        }
+        catch (IOException | ParserConfigurationException | SAXException exception) {
+            exception.printStackTrace();
+        }
+    }
+    
+    // Classes main function is called from the packages main class
+    /*public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException {
+        DomReadDZAE6I dom_read = new DomReadDZAE6I();
+    }*/
     
 }
